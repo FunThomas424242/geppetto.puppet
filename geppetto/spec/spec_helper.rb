e6e531a919b49944823f8ebf2039f39849c81705
Thomas Schubert <funthomas424242@gmail.com>
2014-09-20 23:41:42 +0200

    rb fileformat fixed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- geppetto/spec/spec_helper.rb
+++ geppetto/spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'rspec-puppet'
-2 
-3 fixture_path = File.expand_path(File.join(__FILE__, '..', '..', 'fixtures'))
-4 
-5 RSpec.configure do |c|
-6   c.module_path = File.join(fixture_path, 'modules')
-7   c.manifest_dir = File.join(fixture_path, 'manifests')
-8 end
+ 
+fixture_path = File.expand_path(File.join(__FILE__, '..', '..', 'fixtures'))
+ 
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+end
