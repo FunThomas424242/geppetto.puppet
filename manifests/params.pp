d886e1e0b49867f3478962e81716bee2ac60bc8c
FunThomas424242 <funthomas424242@gmail.com>
2015-10-05 21:23:56 +0000

    parameter defaults fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,50 +3,25 @@
 # This class defines some parameters
 #
 class geppetto::params (
-  $user = undef,
-  $geppettoVersion = undef,
-  $tmpDir = undef,
-  $srcURL = undef,
-  $homeDir = undef,
+  $user = $::id,
+  $geppettoVersion = '4.2.0-R201407250959',
+  $tmpDir = '/tmp',
+  $homeDir = "/home/$::id",
   $libName = undef,
-  $targetDir = undef,
-  $checksum = undef,
+  $targetDir = "/home/$::id/installationen",
+  $checksum = false,
 )
-
-
 {
-
-  $user = undef ? {
-    default => $::id
-  }
- 
-  $geppettoVersion = undef ? {
-    default => '4.2.0-R201407250959'
-  }
-  
-  $tmpDir = undef ? {
-    default => '/tmp'
-  }
-  
   
   $srcURL = undef ?{
     default => "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-linux.gtk.x86_64-$geppettoVersion.zip"
   }       
   
-  $homeDir = undef ? {
-    default => "/home/$user"
-  }
   
   $libName = undef ? {
     default => "geppetto-$geppettoVersion"
   }   
   
-  $targetDir = undef ?{
-    default => "$homeDir/installationen"
-  }
-  
-  $checksum = undef ? {
-    default => false
-  }   
+   
  
 }
