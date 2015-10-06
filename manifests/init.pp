afa6acb5d0e6c41f40c99677a02a98bb5e5a4bfb
FunThomas424242 <funthomas424242@gmail.com>
2015-10-06 16:41:15 +0000

    puppet-lint errors fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,24 +15,18 @@
 
 class geppetto () {
 
- include archive::prerequisites
- 
- include geppetto::params
+  include archive::prerequisites
 
-#  $user            = 'huluvu424242',
-#  $geppettoVersion = '4.2.0-R201407250959',
-#  $tmpDir          = '/tmp',
-#  $srcURL          = undef,
-#  $homeDir         = undef,
-   $libName         = $geppetto::params::libName
-   $targetDir       = $geppetto::params::targetDir
-#  $checksum        = false,
+  include geppetto::params
+
+  $libName         = $geppetto::params::libName
+  $targetDir       = $geppetto::params::targetDir
 
 
-  notice("srcURL: $geppetto::params::srcURL")
-  notice("homeDir: $geppetto::params::homeDir")
-  notice("libName: $geppetto::params::libName")
-  notice("targetDir: $geppetto::params::targetDir")
+  notice("srcURL: ${geppetto::params::srcURL}")
+  notice("homeDir: ${geppetto::params::homeDir}")
+  notice("libName: ${geppetto::params::libName}")
+  notice("targetDir: ${geppetto::params::targetDir}")
 
   file { $geppetto::params::targetDir:
     ensure => directory,
@@ -45,23 +39,26 @@ class geppetto () {
   archive { $geppetto::params::libName:
     ensure     => present,
     url        => $geppetto::params::srcURL,
-    extension => "zip",
+    extension  => 'zip',
     src_target => $geppetto::params::tmpDir,
     target     => $geppetto::params::targetDir,
     checksum   => $geppetto::params::checksum,
-    require    => [File[$geppetto::params::tmpDir], File[$geppetto::params::targetDir]],
+    require    => [File[$geppetto::params::tmpDir]
+      , File[$geppetto::params::targetDir]
+    ],
   }
 
   file { 'geppetto.desktop.Schreibtisch':
     ensure  => file,
-    path    => "$geppetto::params::homeDir/Schreibtisch/geppetto.desktop",
+    path    => "${geppetto::params::homeDir}/Schreibtisch/geppetto.desktop",
     content => template('geppetto/geppetto.desktop.erb'),
     require => Archive[$geppetto::params::libName],
   }
   
   file { 'geppetto.desktop.local':
     ensure  => file,
-    path    => "$geppetto::params::homeDir/.local/share/applications/geppetto.desktop",
+    path    => "${geppetto::params::homeDir}
+    /.local/share/applications/geppetto.desktop",
     content => template('geppetto/geppetto.desktop.erb'),
     require => Archive[$geppetto::params::libName],
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,21 +6,19 @@ class geppetto::params (
   $user = $::id,
   $geppettoVersion = '4.2.0-R201407250959',
   $tmpDir = '/tmp',
-  $homeDir = "/home/$::id",
-  $targetDir = "/home/$::id/installationen",
+  $homeDir = "/home/${::id}",
+  $targetDir = "/home/${::id}/installationen",
   $checksum = false,
 )
 {
   
   $srcURL = undef ?{
-    default => "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-linux.gtk.x86_64-$geppettoVersion.zip"
-  }       
+    default => "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-linux.gtk.x86_64-${geppettoVersion}.zip"
+  }
   
   
   $libName = undef ? {
-    default => "geppetto-$geppettoVersion"
-  }   
-  
-   
- 
+    default => "geppetto-${geppettoVersion}"
+  }
+
 }
